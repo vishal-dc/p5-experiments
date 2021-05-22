08dc3e55a369c1100ccb2c044c82f973038a57fa
Vishal Chaudhari <vichaudh@tibco.com>
2021-05-23 00:07:42 +0530

    stuck in the matrix - got camera movement with asdw keys and mouse

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-let graphics ;
+let graphics;
 let image;
+
+let x = 0, z = 0;
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
@@ -8,41 +10,69 @@ function setup() {
     image.setup();
 }
 
+
 function draw() {
     background(0);
-    ambientLight(255,255,255);
+    ambientLight(0, 255, 0);
     // let v = createVector(mouseX, mouseY, 0);
     let mapMouseX = mouseX - windowWidth / 2;
     let mapMouseY = mouseY - windowHeight / 2;
-    // camera(0, 0, (height / 2) / tan(PI / 6), mapMouseX, mapMouseY, 0, 0, 1, 0);
-
-    // plane(windowWidth, windowHeight);
-    // texture(graphics);
 
 
-    // stroke(100);
+    if (keyIsPressed) {
+        if (x > -windowWidth /2 && (keyCode === LEFT_ARROW || keyCode === 65 )) {
+            x -= 15;
+        } else if (x < windowWidth /2 && (keyCode === RIGHT_ARROW || keyCode === 68)) {
+            x += 15;
+        } else if (keyCode === UP_ARROW || keyCode === 87) {
+            z -= 10;
+        } else if (keyCode === DOWN_ARROW || keyCode === 83) {
+            z += 10;
+        }
+    }
 
+
+    camera(x, 0, (height / 2) / tan(PI / 6)  + z, mapMouseX+ x , mapMouseY, 0, 0, 1, 0);
+
+    texture(graphics);
+
+    // front plane
     push();
-    ambientMaterial(175);
+    translate(0, 0, -windowWidth / 2);
+    plane(windowWidth, windowHeight);
+    pop();
+
+
+    // bottom plane
+    push();
     translate(0, windowHeight / 2, 0);
     rotateX(radians(90));
     plane(windowWidth, windowWidth);
     pop();
 
-    texture(graphics);
+    // top plane
+    push();
+    translate(0, -windowHeight / 2, 0);
+    rotateX(radians(90 + 180));
+    plane(windowWidth, windowWidth);
+    pop();
 
+    // left plane
     push();
     translate(-windowWidth / 2, 0, 0);
     rotateY(radians(90));
-    plane(windowWidth, windowHeight);
+    // rotateX(radians(180));
 
+    plane(windowWidth, windowHeight);
     pop();
 
+    // right plane
     push();
     translate(windowWidth / 2, 0, 0);
     rotateY(radians(-90));
     plane(windowWidth, windowHeight);
     pop();
+
     image.draw();
 
 
@@ -78,7 +108,7 @@ class RainColumn {
     }
 
     draw() {
-        if (this.y > windowHeight ) {
+        if (this.y > windowHeight) {
             this.resetY();
             this.resetX();
             this.resetAlpha();
@@ -119,7 +149,7 @@ class RainColumn {
     }
 
     resetAlpha() {
-        this.alpha =  map(this.x, 0, windowWidth,0.1, 1);
+        this.alpha = map(this.x, 0, windowWidth, 0.1, 1);
     }
 }
 
